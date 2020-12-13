85758241e61d76caf505bcfb6922c2cdb30a56b5
Matthew Molloy <whamtet@gmail.com>
2020-12-14 01:30:47 +0700

    added defcomponent

--- src/clj/clj_htmx/core.clj
+++ src/clj/clj_htmx/core.clj
@@ -13,6 +13,8 @@
     m
     ks))
 
+;; we don't need to expand endpoints here
+;; this is only tagging map values.
 (defn assign-endpoint [x]
   (if (and x (not (string? x)))
     (with-meta x {:endpoint (str (gensym))})
@@ -22,16 +24,39 @@
     (updates m assign-endpoint endpoints)
     m))
 
+(defn- component-macro? [x]
+  (and
+    (symbol? x)
+    (resolve x)
+    (let [evaluated (-> x resolve var-get)]
+      (and
+        (map? evaluated)
+        (contains? evaluated :fn)
+        (contains? evaluated :endpoints)))))
+
 (defn expand-to-string [x]
-  (or (-> x meta :endpoint) x))
+  (or
+    (-> x meta :endpoint)
+    (when (component-macro? x)
+      `(:fn ~x))
+    x))
 
 (defn extract-endpoint [k v]
   (when-let [endpoint (-> v meta :endpoint)]
     [(str "/" endpoint) {(-> k name (.replace "hx-" "") keyword) v}]))
 (defn extract-endpoints [m]
   (cond
-    (map? m) (map #(extract-endpoint % (m %)) endpoints)
-    (coll? m) (mapcat extract-endpoints m)))
+    (map? m) (->> endpoints
+                  (map #(extract-endpoint % (m %)))
+                  (filter identity))
+    (coll? m) (mapcat extract-endpoints m)
+    (component-macro? m) (-> m eval :endpoints)))
+
+(defmacro defcomponent [name & rest]
+  (let [encoded (walk/postwalk assign-endpoints rest)]
+    `(def ~name
+       {:fn (fn ~@(walk/postwalk expand-to-string encoded))
+        :endpoints ~(vec (extract-endpoints encoded))})))
 
 (defmacro make-routes [root f]
   (let [encoded (walk/postwalk assign-endpoints f)]
